06109033b58ec4383c5a14bb2a1dfbf85cb1005a
craigshall <craigsh@gmail.com>
2019-04-18 15:33:14 -0700

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,11 @@ from serve import useless_function
 
 app=Flask(__name__)
 
-# Define our "ping" end point
-@app.route('/ping')
-def useless_output():
-  return(useless_function())
+# Define our "single_vals" end point
+@app.route('/single_vals')
+# read in a processor from our pickled file. Don't forget to 
+# include "rb", which lets us _r_ead a _b_inary file.
+pickle_single_vals = pickle.load(open("single_vals.pkl", "rb"))
+
+# apply it to some sample text to make sure it works
+apply_single_val(pickle_single_vals, weather_df) 
