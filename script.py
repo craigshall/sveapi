51bd79934eb378a2073662d4e27f82c0046aa1c7
Craig Hall <craigsh@gmail.com>
2019-04-18 16:37:11 -0700

    edited serve.py to load pickle and run it; edited script.py to call run_single_val from serve.py

--- script.py
+++ script.py
@@ -1,14 +1,9 @@
 from flask import Flask
-from serve import apply_single_val  
+from serve import run_single_val
 
 app=Flask(__name__)
 
 # Define our "single_vals" end point
 @app.route('/single_vals')
 def single_val_output(input_df):
-    # read in a processor from our pickled file. Don't forget to 
-    # include "rb", which lets us _r_ead a _b_inary file.
-    pickle_single_vals = pickle.load(open("single_vals.pkl", "rb"))
-
-    # apply it to some sample text to make sure it works
-    apply_single_val(pickle_single_vals, input_df) 
+    run_single_val(input_df)
--- serve.py
+++ serve.py
@@ -1,5 +1,10 @@
-def useless_function():
-    """ 
-    A function that returns some text
-    """
-    return("some text")
+import pickle
+
+
+def run_single_val(input_df):
+    # read in a processor from our pickled file. Don't forget to
+    # include "rb", which lets us _r_ead a _b_inary file.
+    pickle_single_vals = pickle.load(open("single_vals.pkl", "rb"))
+
+    return pickle_single_vals(input_df)
+
