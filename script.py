8b8afe4afc7cc21d2a99cdd2a003439045da94ca
Craig Hall <craigsh@gmail.com>
2019-04-19 15:17:02 -0700

    added methods=['POST'] parameter to flask decorator call

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ from serve import run_single_val
 app=Flask(__name__)
 
 # Define our "extractsinglevalues" end point
-@app.route('/extractsinglevalues')
+@app.route('/extractsinglevalues', methods=['POST'])
 def single_val_output(json_df):
     run_single_val(json_df)
