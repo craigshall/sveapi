6f9184f660d1245342b75c08569c2fe3313928ee
Craig Hall <craigsh@gmail.com>
2019-04-22 17:33:08 -0700

    run small test and file test with api_test.py...returning expected values

--- api_test.py
+++ api_test.py
@@ -1,12 +1,15 @@
 import requests
 import pandas as pd
 
-# originally ran with a file...this appeared to time out the connection
-#weather_df = pd.read_csv('weather_test.csv')
-
-#run with a tiny dataframe as proof of concept
+# run with a tiny dataframe as proof of concept
 time = '2019-04-02 11:00:00'  # arbitrary time to start the datetime index
 df = pd.DataFrame(index=pd.date_range(time, periods=3, freq='1H'),
                   columns=['col1', 'col2', 'col3'], data=[[1.0, 2.0, 3.0], [1.0, 4.0, 3.0], [1.0, 6.0, 3.0]])
-return_val = requests.post('http://single-val-test-api.herokuapp.com/extractsinglevalues', json=df.to_json())
+return_val = requests.post('http://single-val-test-api.herokuapp.com/extractsinglevalues', json=df.to_json()).json()
+print(return_val)
+
+# run with a larger DataFrame loaded from a csv
+weather_df = pd.read_csv('weather_test.csv')
+
+return_val = requests.post('http://single-val-test-api.herokuapp.com/extractsinglevalues', json=weather_df.to_json()).json()
 print(return_val)
